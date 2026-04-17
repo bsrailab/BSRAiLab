fdeb6f61083f293cebac9dd99dbbeae7928a181d
hoangtu0812 <hoangtupham.info@gmail.com>
2026-04-17 13:11:05 +0700

    Update base path in Vite configuration to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BSRAiArchitecture/', // Thay đổi theo tên repo trên GitHub
+  base: '/BSRAiLab/', // Base path theo tên repo trên GitHub
 })
